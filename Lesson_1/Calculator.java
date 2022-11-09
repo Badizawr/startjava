f6f75272aa82145ff96bc48cfcfe0327f4d85e18
Badizawr <vicodin-13@ya.ru>
2022-11-09 20:17:17 +0500

    calculator

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,10 +1,10 @@
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("1.Калькулятор");
-        int num1 = 13;
-        int num2 = 2;
+        int num1 = 26;
+        int num2 = 3;
         int result = num1;
-        char sign = '%';
+        char sign = '^';
         switch (sign) {
             case '+':
                 result += num2;
