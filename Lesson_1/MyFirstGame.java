b6ce4d1ede236a03f7a099b5c62e8c7ccd5a5c67
Badizawr <vicodin-13@ya.ru>
2022-11-09 21:22:34 +0500

    make change in task my first game

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -7,15 +7,15 @@ public class MyFirstGame {
         String input;
         String str = "";
         int playerNum;
-        int compNum = rnd.nextInt(0,100);
+        int compNum = rnd.nextInt(1,101);
         int count = 1;
         do {
             input = showInputDialog("Угадай число:");
-            playerNum = Integer.parseInt (input);
+            playerNum = Integer.parseInt(input);
             if (playerNum > compNum) {
-                str = ("Число больше того, что загадал компьютер");
+                str = "Число больше того, что загадал компьютер";
             } else if (playerNum < compNum) {
-                str = ("Число меньше того, что загадал компьютер");
+                str = "Число меньше того, что загадал компьютер";
             } else {
                 str = ("Вы победили, для этого вам понадобилось " + count + " ходов");
             }
